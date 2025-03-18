463ebdb10e0e3bc6d476954264155c69a1e6a66e
AbhishekPandey91 <pandeyabhishek8685@gmail.com>
2025-03-18 16:04:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -121,17 +121,17 @@ if uploaded_file is not None:
         st.title("Most Common Words")
         st.pyplot(fig)
 
-        ## EMOJI ANALYSIS
-        emoji_df = helper.emoji_analysis(selected_user , df )
-        st.title("Emoji Analysis")
+        # ## EMOJI ANALYSIS
+        # emoji_df = helper.emoji_analysis(selected_user , df )
+        # st.title("Emoji Analysis")
 
-        col1, col2 = st.columns(2)
-        with col1:
-            st.dataframe(emoji_df)
-        with col2:
-            fig,ax = plt.subplots()
-            ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct='%0.2f')
-            st.pyplot(fig)
+        # col1, col2 = st.columns(2)
+        # with col1:
+        #     st.dataframe(emoji_df)
+        # with col2:
+        #     fig,ax = plt.subplots()
+        #     ax.pie(emoji_df[1].head(),labels=emoji_df[0].head(),autopct='%0.2f')
+        #     st.pyplot(fig)
 
 
 
